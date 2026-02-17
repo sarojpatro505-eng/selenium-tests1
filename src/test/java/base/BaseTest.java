f6bff61bd6c274f9794899d0b74f45e122b75025
sarojpatro505-eng <sarojpatro505@gmail.com>
2026-02-17 12:14:23 +0530

    Headless fix

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,13 +12,27 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Check if headless mode is enabled
+        String headless = System.getProperty("headless");
+
+        if ("true".equals(headless)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://ynmsafety.com/");
     }
 
     @AfterMethod
     public void tearDown() {
-       // driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
